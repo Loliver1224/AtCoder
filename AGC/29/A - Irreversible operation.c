b203849c11912d353c69b94b4758edea55fff03d
Loliver1224 <swim.01060808@gmail.com>
2019-03-16 14:50:09 +0900

    AGC過去問A問題2問説いた

---
+++ AGC/29/A - Irreversible operation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    long i, cnt = 0, ans = 0;
+    char s[200001];
+    scanf("%s", s);
+    for (i = 0; s[i]; i++)
+        s[i] == 'B' ? cnt++ : (ans += cnt);
+    printf("%ld\n", ans);
+    return 0;
+}
---
+++ AGC/30/A - Poisonous Cookies.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+    int a,b,c;
+    scanf("%d%d%d", &a,&b,&c);
+    printf("%d\n", (a+b+1<c) ? a+2*b+1 : b+c);
+    return 0;
+}
