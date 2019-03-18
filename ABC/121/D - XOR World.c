cd4e1d427f0487419ed9c2ff5807a4fffc025e8c
Loliver1224 <swim.01060808@gmail.com>
2019-03-18 17:42:17 +0900

    D問題とけた！！！！

--- ABC/121/D - XOR World.c
+++ ABC/121/D - XOR World.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+long long f(long long X){
+    if(X%2){
+        if (X%4 == 1)
+            return 1;
+        return 0;
+    }else{
+        if (((X-1)%4) == 1)
+            return 1 ^ X;
+        return 0 ^ X;
+    }
+}
 int main(void){
-    long long a, b, ans;
+    long long a, b;
     scanf("%lld %lld", &a, &b);
-    ans = a % 2 ? a ^ ((b - a + 1) % 2 ? b ^ 1 : b ^ 0)
-                : (b - a - 1) % 2 ? b ^ 1 : b ^ 0;
-    printf("%lld\n", ans);
+    printf("%lld\n", f(a-1) ^ f(b));
     return 0;
 }
